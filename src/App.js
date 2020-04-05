aa50928fe551786084c8fe7a62fc647ff4e10419
kate <bkateryna27@icloud.com>
2020-04-04 22:37:07 -0400

    creat first own component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import Person from "./Person/Person";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <Person />
+      </div>
+    );
+    // return React.createElement(
+    //   "h1",
+    //   { className: "App" },
+    //   "Does this work now?"
+    // );
+  }
 }
 
 export default App;
