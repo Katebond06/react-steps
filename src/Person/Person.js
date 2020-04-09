ece89e764e619df2eb2c6134cda8a80bb237737e
kate <bkateryna27@icloud.com>
2020-04-08 20:15:21 -0400

    state, event listening

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-const person = () => {
-  return <p>I am a person! I am {Math.floor(Math.random() * 30)}</p>;
+const person = (props) => {
+  return (
+    <div>
+      <p>
+        I {props.name} I am {props.age} years old.
+      </p>
+      <p>{props.children}</p>
+    </div>
+  );
 };
 
 export default person;
